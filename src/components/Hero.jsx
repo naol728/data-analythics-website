7be692c1cb4ba2896e486da8652e7c466c7fd895
naol <naolmeseret22@gmail.com>
2024-08-13 19:35:21 +0300

    i created 2 componets and i stayle them

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero=()=>{
+    return(
+        <div className="text-white ">
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto
+            text-center flex flex-col justify-center">
+                <p className="text-[#00df9a] font-bold p-2">GROWING WITH DATA ANALYTHICS</p>
+            <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">Grow with data</h1>
+            <div>
+                <p>Fast,flexiable finacing for</p>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default Hero;
