490d780396ced646d91f098cb85482821133e4ca
naol <naolmeseret22@gmail.com>
2024-08-14 22:28:24 +0300

    i add th Footer componet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Navbar from './components/navbar'
 import Hero from './components/Hero'
 import Analythics from './components/Analythics'
 import Newsletter from './components/Newsletter'
 import Card from './components/Card';
+import Footer from './components/Footer'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,6 +15,7 @@ root.render(
     <Analythics />
     <Newsletter />
     <Card />
+    <Footer />
   </React.StrictMode>
 )
 
